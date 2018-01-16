1f83d14e5c4e5f3b53d9dbf88fc805f4d57f2d77
Seul Zarraga Espinosa <seulzarragaespinosa@Seuls-MacBook-Pro.local>
2018-01-15 21:09:24 -0600

    till navigation

---
+++ src/app/2-todos/todos.component.spec.ts
@@ -0,0 +1,53 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+
+import { TodoService } from './todo.service';
+import { TodosComponent } from './todos.component';
+
+//NOTE: I've deliberately excluded this suite from running
+// because the test will fail. This is because we have not 
+// provided the TodoService as a dependency to TodosComponent. 
+// 
+// When you get to Lecture 6 (Providing Dependencies), be sure
+// to remove "x" from "xdescribe" below. 
+
+xdescribe('TodosComponent', () => {
+  let component: TodosComponent;
+  let fixture: ComponentFixture<TodosComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      declarations: [ TodosComponent ],
+      providers: [ TodoService]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodosComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should load todo from the server', () => {
+    
+    let service = TestBed.get(TodoService);
+    // fixture.debugElement.injector.get(TodoService);
+    spyOn(service, 'getTodos').and.returnValue(Observable.from([1,2,3]));
+
+    fixture.detectChanges();
+
+    expect(component.todos.length).toBe(3);
+
+  });
+
+});
---
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -0,0 +1,47 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { UserDetailsComponent } from './user-details.component';
+
+class RouterStub{
+
+  navigate(prams){
+
+  }
+}
+
+class ActivatedRouteStub{
+
+  params: Observable<any> = Observable.empty();
+
+}
+
+describe('UserDetailsComponent', () => {
+  let component: UserDetailsComponent;
+  let fixture: ComponentFixture<UserDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserDetailsComponent ],
+      providers: [
+        {provide: Router, useClass: RouterStub},
+        {provide: ActivatedRoute, useClass: ActivatedRouteStub}
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
